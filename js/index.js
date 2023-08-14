f21631848e4b8ea947e63c6a38b050049ddfa8be
cheininger <christoph_heininger@hotmail.com>
2023-08-14 21:53:18 +0200

    automatic formatting

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
-
 function getComputerChoice() {
   let randomNumber = Math.random();
   let computerSelection;
 
-  if (randomNumber >= 0 && randomNumber < 1/3) {
-    computerSelection = 'rock';
-  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-    computerSelection = 'paper';
-  } else if (randomNumber >= 2/3 && randomNumber < 1) {
-    computerSelection = 'scissors';
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerSelection = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerSelection = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerSelection = "scissors";
   }
 
   return computerSelection;
-};
+}
 
 function getPlayerChoice() {
-  let playerSelection = prompt('Rock, Paper or Scissors? What\'s your play?!');
+  let playerSelection = prompt("Rock, Paper or Scissors? What's your play?!");
 
   playerSelection = playerSelection.toLowerCase();
 
@@ -23,38 +22,38 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock') {
-    if (computerSelection === 'rock') {
-      alert('It\'s a tie! PC and you both picked Rock!');
-      return 'tie';
-    } else if (computerSelection === 'paper') {
-      alert('You lose! PC\' Paper beats your Rock!');
-      return 'PC';
-    } else if (computerSelection === 'scissors') {
-      alert('You win! Your Rock beats PC\'s Scissors!');
-      return 'player';
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      alert("It's a tie! PC and you both picked Rock!");
+      return "tie";
+    } else if (computerSelection === "paper") {
+      alert("You lose! PC' Paper beats your Rock!");
+      return "PC";
+    } else if (computerSelection === "scissors") {
+      alert("You win! Your Rock beats PC's Scissors!");
+      return "player";
     }
-  } else if (playerSelection === 'paper') {
-    if (computerSelection === 'rock') {
-      alert('You win! Your Paper beats PC\'s Rock!');
-      return 'player';
-    } else if (computerSelection === 'paper') {
-      alert('It\'s a tie! PC and you both picked Paper!');
-      return 'tie';
-    } else if (computerSelection === 'scissors') {
-      alert('You lose! PC\'s Scissors beats your Paper!');
-      return 'PC';
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      alert("You win! Your Paper beats PC's Rock!");
+      return "player";
+    } else if (computerSelection === "paper") {
+      alert("It's a tie! PC and you both picked Paper!");
+      return "tie";
+    } else if (computerSelection === "scissors") {
+      alert("You lose! PC's Scissors beats your Paper!");
+      return "PC";
     }
-  } else if (playerSelection === 'scissors') {
-    if (computerSelection === 'rock') {
-      alert('You lose! PC\'s Rock beats your Scissors');
-      return 'PC';
-    } else if (computerSelection === 'paper') {
-      alert('You win! Your Scissors beats PC\'s Paper!');
-      return 'player';
-    } else if (computerSelection === 'scissors') {
-      alert('It\'s a tie! PC and you both picked Scissors!');
-      return 'tie';
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      alert("You lose! PC's Rock beats your Scissors");
+      return "PC";
+    } else if (computerSelection === "paper") {
+      alert("You win! Your Scissors beats PC's Paper!");
+      return "player";
+    } else if (computerSelection === "scissors") {
+      alert("It's a tie! PC and you both picked Scissors!");
+      return "tie";
     }
   }
 }
@@ -71,21 +70,20 @@ function game() {
 
     result = playRound(playerSelection, computerSelection);
 
-    if (result === 'player') {
+    if (result === "player") {
       playerScore++;
-    } else if (result === 'PC') {
+    } else if (result === "PC") {
       computerScore++;
     }
   }
 
   if (playerScore > computerScore) {
-    alert('Congratulations! You won the Best of 5!');
+    alert("Congratulations! You won the Best of 5!");
   } else if (playerScore < computerScore) {
-    alert('Sorry... The PC won the Best of 5. Better luck next time!');
+    alert("Sorry... The PC won the Best of 5. Better luck next time!");
   } else if (playerScore == computerScore) {
-    alert('The Best of 5 did not result in a winner. Thanks for playing!');
+    alert("The Best of 5 did not result in a winner. Thanks for playing!");
   }
-
 }
 
 game();
